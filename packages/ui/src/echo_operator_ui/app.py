b005413feb92fc7c4fed855a16942f7df801a89f
Ken Chau <kchau@microsoft.com>
2025-03-03 10:08:13 -0800

    added a dummy UI

--- packages/ui/src/echo_operator_ui/app.py
+++ packages/ui/src/echo_operator_ui/app.py
@@ -1,7 +1,19 @@
 import gradio as gr
+def handle_message(message, history):
+    # This function will be called when the user sends a message, it should be fed into agent
+    # with results returned to the user
+    
+    return message
 
-with gr.Blocks() as app:
-    gr.Markdown("# test ui")
+with gr.Blocks(theme=gr.themes.Ocean()) as app:
+    gr.Markdown("# Echo Operator")
+    with gr.Row():
+        with gr.Column(scale=5):
+            gr.Markdown("## Operator Chat")
+            gr.ChatInterface(handle_message, chatbot=gr.Chatbot(label="Echo Operator"), type="messages")
+        with gr.Column(scale=7):
+            gr.Markdown("## Agent Screen")
+            
 
 if __name__ == "__main__":
     app.launch()
