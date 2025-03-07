efddb87d5651be1f484e90e9edb00d6bf94b8df6
lukaskellerstein <lukas.kellerstein@gmail.com>
2025-03-07 17:26:55 +0100

    add magma model with video

--- models/magma/README.md
+++ models/magma/README.md
@@ -1,12 +1,21 @@
 # Magma model
 
 Github: https://github.com/microsoft/Magma
+
 HuggingFace: https://huggingface.co/microsoft/Magma-8B
 
 ## Installation
 
-`uv sync`
+Run `uv sync`
 
 ## Run
 
-`uv run main.py`
+### Image
+
+To analyse image, run `uv run main_image.py`.
+
+You should see output in terminal as follows: `The time displayed on the computer is 12:27 PM.`
+
+### Video
+
+To analyse video, run `uv run main_video.py`.
--- models/magma/main_video.py
+++ models/magma/main_video.py
@@ -2,6 +2,9 @@ from PIL import Image
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoProcessor 
+from helpers import extract_frames
+import os
+import cv2
 
 dtype = torch.bfloat16
 model = AutoModelForCausalLM.from_pretrained("microsoft/Magma-8B", trust_remote_code=True, torch_dtype=dtype)
@@ -9,14 +12,18 @@ processor = AutoProcessor.from_pretrained("microsoft/Magma-8B", trust_remote_cod
 model.to("cuda")
 
 # Inference
-image = Image.open("./img/magma_logo.jpg").convert("RGB")
+video = "./video/recording.mp4"
 
+frames = extract_frames(video)
+print(f"Extracted {len(frames)} frames.")
+
+image_tokens = "\n".join(["<image_start><image><image_end>"] * len(frames))
 convs = [
-    {"role": "system", "content": "You are agent that can see, talk and act."},            
-    {"role": "user", "content": "<image_start><image><image_end>\nWhat is the letter on the robot?"},
+    {"role": "system", "content": "You are an agent that can see, talk, and act."},            
+    {"role": "user", "content": f"{image_tokens}\nWhat application got opened?"},
 ]
 prompt = processor.tokenizer.apply_chat_template(convs, tokenize=False, add_generation_prompt=True)
-inputs = processor(images=[image], texts=prompt, return_tensors="pt")
+inputs = processor(images=frames, texts=prompt, return_tensors="pt")
 inputs['pixel_values'] = inputs['pixel_values'].unsqueeze(0)
 inputs['image_sizes'] = inputs['image_sizes'].unsqueeze(0)
 inputs = inputs.to("cuda").to(dtype)
@@ -30,9 +37,16 @@ generation_args = {
 } 
 
 with torch.inference_mode():
+    print("Generating response...")
     generate_ids = model.generate(**inputs, **generation_args)
+    print("Response generated.")
 
+
+print("Decoding response...")
 generate_ids = generate_ids[:, inputs["input_ids"].shape[-1] :]
 response = processor.decode(generate_ids[0], skip_special_tokens=True).strip()
+print("Response decoded.")
 
+print("------------------------------")
+print("Response:")
 print(response)
