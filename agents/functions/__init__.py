ff162e7b0e13f612bd37ef277f69df39645c776a
kchau@microsoft.com <kchau@microsoft.com>
2025-03-26 15:08:28 -0700

    clean up file before we start a run

--- agents/functions/__init__.py
+++ agents/functions/__init__.py
@@ -3,6 +3,7 @@ from functions.default.open_application import open_application
 from functions.default.start_network_proxy import start_network_proxy
 from functions.default.stop_network_proxy import stop_network_proxy
 from functions.default.get_evaluation_results import get_evaluation_results
+from functions.default.delete_files import delete_files
 
 FUNCTIONS = {
     "close_all_windows": close_all_windows,
@@ -10,4 +11,5 @@ FUNCTIONS = {
     "start_network_proxy": start_network_proxy,
     "stop_network_proxy": stop_network_proxy,
     "get_evaluation_results": get_evaluation_results,
+    "delete_files": delete_files,
 }
